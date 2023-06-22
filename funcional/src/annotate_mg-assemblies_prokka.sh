9fc80d3a004b146bb39a4226fcd5a38fcfbe20cb
Anton Pashkov <92653829+aapashkov@users.noreply.github.com>
2023-06-22 09:16:34 -0600

    Delete funcional/src directory

--- funcional/src/annotate_mg-assemblies_prokka.sh
+++
@@ -1,9 +0,0 @@
-# Usage: ./prokka-assemblies.sh
-
-cd $(dirname "$(dirname "$(readlink -f $0)")")
-
-ls data/01-assemblies/01-metagenomes/* | while read assembly_file; do
-  base=$(basename ${assembly_file%%.fasta})
-  prokka --outdir data/02-annotations/01-prokka/01-metagenomes/${base} \
-    --norrna --notrna --prefix ${base} --metagenome --cpus 16 ${assembly_file}
-done
