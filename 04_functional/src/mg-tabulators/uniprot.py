bf1d6036e4e6064a7eed599a83f4212207077534
aapashkov <pashkov@comunidad.unam.mx>
2023-06-25 15:25:37 +0000

    Minor fixes to uniprot and vfdb tabulators

--- 04_functional/src/mg-tabulators/uniprot.py
+++ 04_functional/src/mg-tabulators/uniprot.py
@@ -2,6 +2,7 @@
 
 # Usage:  ./uniprot.py
 
+import math
 import os
 import sys
 from multiprocessing import Pool
@@ -15,25 +16,26 @@ out = base_dir/"data/metagenomic/tables/"
 os.makedirs(out, exist_ok=True)
 counts = []
 
-# Counts the number of times each BLAST result with identity >=70% appears
+# Returns a Pandas series with counts of BLAST results with identity >= 80%
 def process_tsv(file):
-    table = pd.read_csv(file, delimiter="\t")
-    table = table[table["pident"] >= 70.0]
-    table = table["sseqid"].value_counts()
+    table = pd.read_csv(file, delimiter="\t", usecols=["sseqid", "pident"])
+    table = table[table["pident"] >= 80.0]["sseqid"].value_counts()
     table.name = file.stem
     return table
 
-# Use half the CPUs available for the process_tsv function
-with Pool(processes=(os.cpu_count() // 2) + 1) as pool:
+# Use a quarter of the CPUs available for the process_tsv function
+with Pool(processes=math.ceil(os.cpu_count() / 4)) as pool:
     for i, count in enumerate(pool.imap_unordered(process_tsv, inp)):
         progress = (i+1) / len(inp) * 100
         sys.stderr.write(f"UniProt tabulation progress: {progress:.2f}%\r")
         counts.append(count)
 
 # Create output table and save
-print()
+print(f"UniProt tabulation progress: Concatenating...", end="\r")
 data = pd.concat(counts, axis=1).T.fillna(0)
 data["City"] = data.index.str[23:26]
 cols = ["City"] + list(data.columns[:-1])
 data = data.reindex(columns=cols).sort_index()
-data.to_csv(out/"uniprot.tsv", sep="\t", index=True)
+print(f"UniProt tabulation progress: Saving...       ", end="\r")
+data.to_csv(out/"uniprot.tsv.gz", sep="\t", index=True, compression="gzip")
+print(f"UniProt tabulation progress: Done!           ")
