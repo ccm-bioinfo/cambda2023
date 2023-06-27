c83bf6dfe5bcf31c0569048c709d20cd687cc046
inuzm <inuzm@live.com>
2023-06-26 21:17:22 -0600

    added convenience script

---
+++ 02_variable_selection/codes/nb_with_training.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# Run the variable selection using a validation data set 
+# Negative binomial with separated kingdoms
+Rscript variable_selection.R -m nb -a FALSE -O "../selected_variables_results/" -v "../validation_set/train_val.csv"
+cd ../selected_variables_results/pValues/
+rm ../selected_variables_results/pValues/reads_kingdoms_nb_pvalues_tv.csv.gz
+gzip -9 reads_kingdoms_nb_pvalues_tv.csv
+cd ../codes/
