d7790438bcd90eb872cc2553d196be115d5a8ba5
inuzm <inuzm@live.com>
2023-07-04 20:55:27 -0600

    generated alpha diversity plots with the selected variables

---
+++ 02_variable_selection/codes/plot_alpha_diversity.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+Rscript alpha_div_with_selected_variables.R -m p
+Rscript alpha_div_with_selected_variables.R -m nb
+Rscript alpha_div_with_selected_variables.R -m zip
+Rscript alpha_div_with_selected_variables.R -m zinb
+Rscript alpha_div_with_selected_variables.R -m best
+
+Rscript alpha_div_with_selected_variables.R -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -R FALSE -m p
+Rscript alpha_div_with_selected_variables.R -R FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -R FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -R FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -R FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -R FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -R FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -R FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -R FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -R FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -u FALSE -m p
+Rscript alpha_div_with_selected_variables.R -u FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -u FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -u FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -u FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -u FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -u FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -u FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -u FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -u FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -m p
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -u FALSE -R FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -R FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -k TRUE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -m best
+
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -k TRUE -m p
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -k TRUE -m nb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -k TRUE -m zip
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -k TRUE -m zinb
+Rscript alpha_div_with_selected_variables.R -r FALSE -u FALSE -R FALSE -k TRUE -m best
