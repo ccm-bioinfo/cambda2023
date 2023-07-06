92e6706fdf192f67d68b3086a9a21090403fb6cb
aapashkov <pashkov@comunidad.unam.mx>
2023-07-06 21:41:10 +0000

    Fix mifaser tabulator

--- 04_functional/src/tabulators/mifaser.py
+++ 04_functional/src/tabulators/mifaser.py
@@ -14,7 +14,7 @@ try:
 except ValueError:
     genomic = 0
 
-try: inp = list(Path(sys.argv[1]).glob("*/analysis.tsv"))
+try: inp = list(Path(sys.argv[1]).glob("*/ec_count.tsv"))
 except IndexError:
     print("ERROR: Missing input directory", file=sys.stderr)
     sys.exit(1)
@@ -28,30 +28,24 @@ os.makedirs(out, exist_ok=True)
 
 i = 0
 
-# Creates a dictionary of ec: count for each file
+# Creates a series of ec: count for each file
 def load_file(file):
     global i
-    data = pd.read_csv(
-        file, delimiter="\t", names=["ec", "count"], header=0, index_col="ec"
-    )["count"]
+    data = pd.read_csv(file, header=None).sum(axis=1)
+    data = data.str.split("\t", n=1).str.get(1).str.split("\t").explode()
     i += 1
     print(
         f"Mi-faser tabulation progress: {i/(len(inp))*100:.2f}%",
         file=sys.stderr, end="\r"
     )
-    return dict(data)
+    return data.value_counts()
 
 # Create complete table
-print(
-    "Mi-faser tabulation progress: Concatenating...", file=sys.stderr, end="\r"
-)
 table = pd.DataFrame(
     {file.parent.stem: load_file(file) for file in inp}
 ).fillna(0)
 
-print(
-    "Mi-faser tabulation progress: Saving...       ", file=sys.stderr, end="\r"
-)
+print("Mi-faser tabulation progress: Saving...", file=sys.stderr, end="\r")
 
 # Save tables by level
 for j in range(1, 5):
@@ -71,4 +65,4 @@ for j in range(1, 5):
     partial.columns.name = ""
     partial.to_csv(out/f"lvl{j}.tsv", sep="\t", index=True)
 
-print("Mi-faser tabulation progress: Done!           ", file=sys.stderr)
+print("Mi-faser tabulation progress: Done!    ", file=sys.stderr)
