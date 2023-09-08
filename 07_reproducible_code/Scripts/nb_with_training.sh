5b61062c516315f06d29535a7eea65bbd496835a
DanielS <daniel@amphorahealth.com>
2023-09-08 17:50:27 -0600

    fix: :pencil2: typo on file name

--- 07_reproducible_code/Scripts/nb_with_training.sh
+++ 07_reproducible_code/Scripts/nb_with_training.sh
@@ -10,7 +10,7 @@ echo "Testing for R libraries ==================================================
 echo "Running the variable selection with a train data set ========================"
 Rscript Scripts/Variable_selection.R -m nb -a FALSE -O "Data/" -v "Data/train_val.csv"
 echo "preparing the results ======================================================="
-mv reads_kindoms*.csv Outputs/Variable-selection/
+mv reads_kingdoms*.csv Outputs/Variable-selection/
 #cd ../selected_variables_results/pValues/
 #rm reads_kingdoms_nb_pvalues_tv.csv.gz
 #gzip -9 reads_kingdoms_nb_pvalues_tv.csv
